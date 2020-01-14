d33dae073a03aff18928e444b7ffcdd5d3ef34f2
yerinie0526 <50296979+yerinie0526@users.noreply.github.com>
2020-01-14 16:08:30 +0900

    Update TestClass.java

--- TestClass.java
+++ TestClass.java
@@ -4,6 +4,6 @@ public class TestClass{
 
     public static void main (String[] args){
         System.out.println("Hello!!!!!");
-        System.out.println("wefwefwef!");
+        System.out.println("wefwefwef!efjweifwiefhwiehgiwehieifoweigf");
     }
 }
